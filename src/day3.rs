bea42fb3bc92ad88d4f5a9b5287f983cf8a38d66
Oleksii Popravka <popravich@users.noreply.github.com>
2022-12-03 11:37:30 +0200

    Day3

--- src/day3.rs
+++ src/day3.rs
@@ -1,4 +1,60 @@
 
-pub fn main(data: &str) -> (usize, usize) {
-    (0, 0)
+pub fn main(data: &str) -> anyhow::Result<(usize, usize)> {
+    let part1 = data.lines().filter_map(|line| {
+        let n = line.len() / 2;
+        let (left, right) = line.split_at(n);
+        left.chars()
+            .take(n)
+            .filter(|c| right.contains(*c))
+            .next()
+    }).collect::<String>();
+
+    let mut lines = data.lines();
+    let mut part2 = 0;
+    while let Some(first) = lines.next() {
+        let second = lines.next().ok_or(anyhow::anyhow!("invalid input"))?;
+        let third = lines.next().ok_or(anyhow::anyhow!("invalid input"))?;
+        let c = first.chars()
+            .filter(|c| second.contains(*c))
+            .filter(|c| third.contains(*c))
+            .next()
+            .ok_or(anyhow::anyhow!("No common items found"))?;
+        part2 += char_score(c) as usize;
+    }
+
+    Ok((score(&part1), part2))
+}
+
+fn score(s: &str) -> usize {
+    s.chars()
+    .map(|c| char_score(c) as usize)
+    .sum()
+}
+
+fn char_score(c: char) -> u8 {
+    match c as u8 {
+        b'a'..=b'z' => (c as u8) - b'a' + 1,
+        b'A'..=b'Z' => (c as u8) - b'A' + 27,
+        _ => unreachable!("Unexpected char: {}", c),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::main;
+
+    static DATA: &str = concat!(
+        "vJrwpWtwJgWrhcsFMMfFFhFp\n",
+        "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\n",
+        "PmmdzqPrVvPwwTWBwg\n",
+        "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\n",
+        "ttgJtRGJQctTZtZT\n",
+        "CrZsJsPPZsGzwwsLwLmpwMDw",
+    );
+
+    #[test]
+    fn solution() {
+        let res = main(DATA).expect("Invalid input");
+        assert_eq!(res, (157, 70));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use advent2022::{
 
 static DAY1: &str = include_str!("./input/day01.txt");
 static DAY2: &str = include_str!("./input/day02.txt");
-static DAY3: &str = include_str!("./input/day01.txt");
+static DAY3: &str = include_str!("./input/day03.txt");
 static DAY4: &str = include_str!("./input/day01.txt");
 
 fn main() -> anyhow::Result<()> {
@@ -29,9 +29,9 @@ fn main() -> anyhow::Result<()> {
             println!("#2: Real score: {}", answer2);
         }
         3 => {
-            let (answer1, answer2) = day3::main(DAY3);
-            println!("#1: {}", answer1);
-            println!("#2: {}", answer2);
+            let (answer1, answer2) = day3::main(DAY3)?;
+            println!("#1: Priorities sum of duplicates: {}", answer1);
+            println!("#2: Priorities sum of badges: {}", answer2);
         }
         4 => {
             let (answer1, answer2) = day4::main(DAY4);
